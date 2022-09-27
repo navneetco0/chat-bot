17c5058a55a5199a7c6bb038ea3e46f82bb735f3
navneetco0 <navneetharsh3@gmail.com>
2022-09-27 23:39:06 +0530

    working on cors

--- src/components/Home/Chat.jsx
+++ src/components/Home/Chat.jsx
@@ -2,9 +2,11 @@ import io from 'socket.io-client'
 import { NormalBox } from './NormalBox'
 import { FormBox } from './FormBox'
 import { Flex } from '@chakra-ui/react'
-const socket = io.connect('https://chatbot-na.herokuapp.com')
+const ENDPOINT = 'https://chatbot-na.herokuapp.com/';
+let socket;
 
 export const Chat = () => {
+  socket=io(ENDPOINT);
   return (
     <Flex
       w="100%"
