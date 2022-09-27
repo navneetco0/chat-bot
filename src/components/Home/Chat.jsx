8d1602eb6ff8c64f0e57fb6ce1b6cc9dd3797edf
navneetco0 <navneetharsh3@gmail.com>
2022-09-27 21:22:21 +0530

    link updated

--- src/components/Home/Chat.jsx
+++ src/components/Home/Chat.jsx
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 import { NormalBox } from './NormalBox'
 import { FormBox } from './FormBox'
 import { Flex } from '@chakra-ui/react'
-const socket = io.connect('http://localhost:5000')
+const socket = io.connect('https://chatbot-na.herokuapp.com/chats')
 
 export const Chat = () => {
   return (
